ae6f6ca041c654211757c04fab597168b72f7b0f
Juan Montoya <juan.montoya@nearsure.com>
2024-04-19 13:26:25 -0500

    initial commit

---
+++ MinimalAPIPostgreSQLEF/Data/OfficeDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPostgreSQLEF.Models;
+
+namespace MinimalAPIPostgreSQLEF.Data;
+
+public class OfficeDBContext: DbContext
+{
+    public OfficeDBContext(DbContextOptions<OfficeDBContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Employee> Employees => Set<Employee>();
+}
